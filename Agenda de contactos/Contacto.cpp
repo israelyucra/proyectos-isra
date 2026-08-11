424118255e4295e64511b076a7c13a6c73a6dd3d
israel yucra <notebookisra@outlook.com>
2026-08-11 05:26:53 -0300

    ahora se desarrollo los setters para los atributos

--- Agenda de contactos/Contacto.cpp
+++ Agenda de contactos/Contacto.cpp
@@ -21,3 +21,31 @@ Contacto::Contacto(const char* _Nombre, const char* _Telefono, const char* _Emai
     SetCategoria(_Categoria);
     Estado = true;
 }
+
+// setters
+void Contacto::SetNombre(const char* nombre)
+{
+    strncpy(Nombre, nombre, sizeof(Nombre) - 1);
+    Nombre[sizeof(Nombre) - 1] = '\0';
+}
+void Contacto::SetTelefono(const char* telefono)
+{
+    strncpy(Telefono, telefono, sizeof(Telefono) - 1);
+    Telefono[sizeof(Telefono) - 1] = '\0';
+}
+void Contacto::SetEmail(const char* email)
+{
+    strncpy(Email, email, sizeof(Email) - 1);
+    Email[sizeof(Email) - 1] = '\0';
+}
+void Contacto::SetDireccion(const char* direccion)
+{
+    strncpy(Direccion, direccion, sizeof(Direccion) - 1);
+    Direccion[sizeof(Direccion) - 1] = '\0';
+}
+void Contacto::SetCategoria(const char* categoria)
+{
+    strncpy(Categoria, categoria, sizeof(Categoria) - 1);
+    Categoria[sizeof(Categoria) - 1] = '\0';
+}
+void Contacto::SetEstado(bool estado) { Estado = estado; }
